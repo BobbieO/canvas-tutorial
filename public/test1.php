33aa1bdea3af23a560f3a68b6ca296b7dadaa1a4
BobbieO <bobbie2504@msn.com>
2016-07-14 14:04:26 -0500

    Examples added

---
+++ public/test1.php
@@ -0,0 +1,37 @@
+<?php
+
+$username = "Fred Smith";
+echo $username;
+echo "<br>";
+$current_user = $username;
+echo $current_user;
+echo "<br>";
+
+
+$oxo = (array( array('x', '', 'o'), array('o', 'o', 'x'), array('x', 'o', '') ) );
+
+echo $oxo[1][2];
+echo "<br>";
+
+
+$author = "Steve Ballmer";
+
+echo "Developers, Developers, developers, developers, developers, developers, developers, developers, developers, developers, developers!
+
+
+
+-$author.";
+
+echo "<br>";
+
+
+$new_author = "Bill Gates";
+
+$text = "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.
+
+- $new_author.";
+
+echo $text;
+
+
+?>
